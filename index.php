bba9a8e9612ba30e88e7c0e3bb14863d06f9dd2a
catensports <62461296+catensports@users.noreply.github.com>
2020-05-28 13:24:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include('login.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+header("location: profile.php"); // Redirecting To Profile Page
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form in PHP with Session</title>
+<link href="style1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>UserName :</label>
+<input id="name" name="username" placeholder="username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password"><br><br>
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+<a href="register.php">register</a>
+</body>
+</html>
